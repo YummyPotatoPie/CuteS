60cba8d66145a5f0d087b96bcf2435c8abc4886d
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-07-16 22:11:30 +0300

    Added symbol streams

---
+++ Lexer/SymbolStreams/ISymbolStream.cs
@@ -0,0 +1,26 @@
+﻿namespace Lexer.SymbolStreams
+{
+    /// <summary>
+    /// Interface for symbols streams
+    /// </summary>
+    public interface ISymbolStream
+    {
+        /// <summary>
+        /// Returns current symbol at the stream
+        /// </summary>
+        /// <returns>Current stream symbol</returns>
+        public char PeekSymbol();
+
+        /// <summary>
+        /// Returns next symbol at the stream
+        /// </summary>
+        /// <returns>Next stream symbol. If stream reached the end of file returns NUL (0)</returns>
+        public char NextSymbol();
+
+        /// <summary>
+        /// Checks if the stream has reached the end of the file
+        /// </summary>
+        /// <returns>Returns true if stream reaches the end of file, otherwise returns false</returns>
+        public bool EndOfStream();
+    }
+}
