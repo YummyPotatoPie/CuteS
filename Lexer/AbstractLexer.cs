16cc68e29e228f17aca9d789959733b44390fa79
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-07-16 22:12:58 +0300

    Added lexer

---
+++ Lexer/AbstractLexer.cs
@@ -0,0 +1,34 @@
+﻿using Lexer.Tokens;
+using Lexer.SymbolStreams;
+
+namespace Lexer
+{
+    /// <summary>
+    /// Class of the abstract lexer containing special fields
+    /// </summary>
+    public abstract class AbstractLexer : ILexer
+    {
+        /// <summary>
+        /// Symbol stream implementing ISymbolStream interface
+        /// </summary>
+        protected ISymbolStream SymbolStream;
+
+        /// <summary>
+        /// Current line 
+        /// </summary>
+        protected int Line = 1;
+
+        /// <summary>
+        /// Current symbol position at the current line
+        /// </summary>
+        protected int SymbolPosition = 1;
+
+        /// <summary>
+        /// Base AbstractLexer class constructor
+        /// </summary>
+        /// <param name="symbolStream">Any symbol stream that implements ISymbolStream interface</param>
+        public AbstractLexer(ISymbolStream symbolStream) => SymbolStream = symbolStream;
+
+        public abstract Token GetNextToken();
+    }
+}
---
+++ Lexer/SimpleLexer.cs
@@ -0,0 +1,94 @@
+﻿using Lexer.SymbolStreams;
+using Lexer.Tokens;
+using Lexer.Exceptions;
+
+namespace Lexer
+{
+    /// <summary>
+    /// Simple lexer using not the fastest algorithms
+    /// </summary>
+    public class SimpleLexer : AbstractLexer
+    {
+        /// <summary>
+        /// Base constructor of SimpleLexer class
+        /// </summary>
+        /// <param name="symbolStream"></param>
+        public SimpleLexer(ISymbolStream symbolStream) : base(symbolStream) { }
+
+        /// <summary>
+        /// Skip comment line
+        /// </summary>
+        private void SkipComment()
+        {
+            while (SymbolStream.PeekSymbol() != '\n') SymbolStream.NextSymbol();
+           
+            Line++;
+            SymbolPosition = 1;
+            if (SymbolStream.NextSymbol() == '#') SkipComment();
+        }
+
+        /// <summary>
+        /// Checks if symbol is a operator symbol
+        /// </summary>
+        /// <param name="symbol">Current symbol at symbol stream</param>
+        /// <returns></returns>
+        private bool IsOperatorSymbol(char symbol) => "!<>=-|&".IndexOf(symbol) > -1;
+        
+        /// <summary>
+        /// Returns token representation of the operator
+        /// </summary>
+        /// <returns>Token representing operator</returns>
+        private Token ReadOperator()
+        {
+            switch (SymbolStream.PeekSymbol())
+            {
+                case '&':
+                    if (SymbolStream.NextSymbol() == '&') return WordToken.AND;
+                    else return new Token('&');
+                case '|':
+                    if (SymbolStream.NextSymbol() == '|') return WordToken.OR;
+                    else return new Token('|');
+                case '=':
+                    if (SymbolStream.NextSymbol() == '=') return WordToken.EQ;
+                    else return new Token('=');
+                case '!':
+                    if (SymbolStream.NextSymbol() == '=') return WordToken.NE;
+                    else return new Token('!');
+                case '<':
+                    if (SymbolStream.NextSymbol() == '=') return WordToken.LE;
+                    else return new Token('<');
+                case '>':
+                    if (SymbolStream.NextSymbol() == '=') return WordToken.GE;
+                    else return new Token('>');
+                default:
+                    throw new SyntaxError($"Expected operator: line {Line}, position {SymbolPosition}");
+            }
+        }
+
+        /// <summary>
+        /// Skips whitespaces
+        /// </summary>
+        private void SkipWhiteSpaces()
+        {
+            char currentSymbol = SymbolStream.PeekSymbol();
+            while (char.IsWhiteSpace(currentSymbol))
+            {
+                if (currentSymbol == '\n')
+                {
+                    Line++;
+                    SymbolPosition = 0;
+                }
+                currentSymbol = SymbolStream.NextSymbol();
+                SymbolPosition++;
+            }
+        }
+
+        public override Token GetNextToken()
+        {
+            SkipWhiteSpaces();
+            if (SymbolStream.PeekSymbol() == '#') SkipComment();
+            if (IsOperatorSymbol(SymbolStream.PeekSymbol())) return ReadOperator();
+            throw new System.NotImplementedException();
+        }
+    }
+}
