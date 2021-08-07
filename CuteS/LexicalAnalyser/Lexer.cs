426ef1ab761d9da59aa6166707cd73afa23bf5d8
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-08-07 20:38:38 +0300

    Some changes

--- CuteS/LexicalAnalyser/Lexer.cs
+++ CuteS/LexicalAnalyser/Lexer.cs
@@ -13,11 +13,11 @@ namespace CuteS.LexicalAnalyser
 
         private int _position = 0;
 
-        private Hashtable _keywords = new();
-
         private List<char> currentLineBuffer = new();
 
         public int Line { get; private set; } = 1;
+
+        public Hashtable IdentifiersTable { get; private set; } = new();
 
         public Lexer(string stream)
         {
@@ -29,20 +29,36 @@ namespace CuteS.LexicalAnalyser
             ReserveWord(WordToken.Import);
         }
 
-        private void ReserveWord(WordToken word) => _keywords.Add(word.Lexeme, word);
+        private void ReserveWord(WordToken word) => IdentifiersTable.Add(word.Lexeme, word);
 
         private void SkipWhiteSpaces()
         {
             while (_position != _stream.Length && char.IsWhiteSpace(_stream[_position]))
             {
-                currentLineBuffer.Add(_stream[_position]);
                 if (_stream[_position] == '\n')
                 {
                     Line++;
                     currentLineBuffer.Clear();
+                    _position++;
+                    continue;
                 }
+                if (_stream[_position] == '\t')
+                {
+                    for (int i = 0; i < 4; i++) currentLineBuffer.Add(' ');
+                    _position++;
+                    continue;
+                }
+                currentLineBuffer.Add(_stream[_position]);
                 _position++;
             }
+        }
+
+        private bool IsOperatorSymbol(char symbol) => "~!%^&*-+=|/<>:".IndexOf(symbol) > -1;
+
+        private Token NextOperator()
+        {
+            // TODO: Implementation 
+            throw new NotImplementedException();
         }
 
         private WordToken NextWord()
@@ -57,7 +73,13 @@ namespace CuteS.LexicalAnalyser
                     currentLineBuffer.Add(_stream[_position]);
                     _position++;
                 }
-                return new WordToken(buffer.ToString());
+
+                WordToken token = (WordToken) IdentifiersTable[buffer.ToString()];
+                if (token != null) return token;
+
+                token = new(buffer.ToString());
+                IdentifiersTable.Add(token.Lexeme, token);
+                return token;
             }
             catch (ArgumentOutOfRangeException)
             {
@@ -65,14 +87,91 @@ namespace CuteS.LexicalAnalyser
             }
         }
 
-        public Token NextToken()
+        private Token NextNumber()
+        {
+            int value = 0;
+            while (_position != _stream.Length && char.IsDigit(_stream[_position]))
+            {
+                currentLineBuffer.Add(_stream[_position]);
+                value = value * 10 + _stream[_position] - '0';
+                _position++;
+            }
+
+            if (_position != _stream.Length && _stream[_position] == '.')
+            {
+                float floatValue = value;
+                int divisor = 10;
+
+                currentLineBuffer.Add(_stream[_position]);
+                _position++;
+                while (_position != _stream.Length && char.IsDigit(_stream[_position]))
+                {
+                    currentLineBuffer.Add(_stream[_position]);
+                    floatValue += (_stream[_position] - '0') / divisor;
+                    divisor *= 10;
+                    _position++;
+                }
+
+                if (divisor == 10) throw new LexerError(currentLineBuffer, "Expected number", Line);
+                return new Token((int)floatValue);
+
+            }
+            return new Token(value);
+        }
+
+        /// <summary>
+        /// Enables if lexer finds error at input stream
+        /// </summary>
+        /// <returns>Errors messages</returns>
+        private string PanicMode()
+        {
+            StringBuilder errorMessages = new();
+
+            Token token = null;
+            do
+            {
+                try
+                {
+                    token = ReadNextToken();
+                }
+                catch (Exception ex)
+                {
+                    errorMessages.Append(ex.Message).Append("\n\n");
+                }
+            } while (token != null);
+
+            return errorMessages.ToString();
+        }
+
+        private Token ReadNextToken()
         {
             SkipWhiteSpaces();
             if (_position == _stream.Length) return null;
 
             char currentSymbol = _stream[_position];
             if (char.IsLetter(currentSymbol) || currentSymbol == '_') return NextWord();
-            return null;
+            if (char.IsDigit(currentSymbol)) return NextNumber();
+            if (IsOperatorSymbol(currentSymbol)) return NextOperator();
+
+            currentLineBuffer.Add(_stream[_position]);
+            _position++;
+            return new Token(currentSymbol);
+        }
+
+        public Token NextToken()
+        {
+            try
+            {
+                return ReadNextToken();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Compilation error:\n{ex.Message}\n\n{PanicMode()}");
+                Environment.Exit(1);
+                
+                // Useless return statements just needed to compile this without errors
+                return null;
+            }
         }
     }
 }
