baee349ad1bff9d907bb81fa4c7017e667b1383b
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-08-08 21:53:53 +0300

    Added operators representation

--- CuteS/LexicalAnalyser/Tokens/WordToken.cs
+++ CuteS/LexicalAnalyser/Tokens/WordToken.cs
@@ -6,9 +6,46 @@
 
         // Reserved words 
         public readonly static WordToken
-            Namespace = new("namespace", TokenAttributes.Namespace),
-            Class = new("class", TokenAttributes.Class),
-            Import = new("import", TokenAttributes.Import);
+            Namespace   = new("namespace", TokenAttributes.Namespace),
+            Class       = new("class", TokenAttributes.Class),
+            Import      = new("import", TokenAttributes.Import);
+
+        // Operators
+        public readonly static WordToken
+            AddAssign       = new("+=", TokenAttributes.AddAssign),
+            SubAssign       = new("-=", TokenAttributes.SubAssign),
+            DivAssign       = new("/=", TokenAttributes.DivAssign),
+            MulAssign       = new("*=", TokenAttributes.MulAssign),
+            ModAssign       = new("%=", TokenAttributes.ModAssign),
+            RShiftAssign    = new(">>=", TokenAttributes.RShiftAssign),
+            LShiftAssign    = new("<<=", TokenAttributes.LShiftAssign),
+            BitAndAssign    = new("&=", TokenAttributes.BitAndAssign),
+            BitOrAssign     = new("|=", TokenAttributes.BitOrAssign),
+            XorAssign       = new("^=", TokenAttributes.XorAssign),
+            BitNotAssign    = new("~=", TokenAttributes.BitNotAssign),
+            NotEq           = new("!=", TokenAttributes.NotEq),
+            Eq              = new("==", TokenAttributes.Eq),
+            GreaterEq       = new(">=", TokenAttributes.GreaterEq),
+            LowerEq         = new("<=", TokenAttributes.LowerEq),
+            Assign          = new("=", TokenAttributes.Assign),
+            Add             = new("+", TokenAttributes.Add),
+            Sub             = new("-", TokenAttributes.Sub),
+            Div             = new("/", TokenAttributes.Div),
+            Mul             = new("*", TokenAttributes.Mul),
+            Mod             = new("%", TokenAttributes.Mod),
+            RShift          = new(">>", TokenAttributes.RShift),
+            LShift          = new("<<", TokenAttributes.LShift),
+            BitOr           = new("|", TokenAttributes.BitAnd),
+            BoolOr          = new("||", TokenAttributes.BoolOr),
+            BitAnd          = new("&", TokenAttributes.BitAnd),
+            BoolAnd         = new("&&", TokenAttributes.BoolAnd),
+            Xor             = new("^", TokenAttributes.Xor),
+            BitNot          = new("~", TokenAttributes.BitNot),
+            BoolNot         = new("!", TokenAttributes.BoolNot),
+            TypeOp          = new(":", TokenAttributes.TypeOp),
+            DotOp           = new(".", TokenAttributes.DotOp),
+            Greater         = new(">", TokenAttributes.Greater),
+            Lower           = new("<", TokenAttributes.Lower);
 
         /// <summary>
         /// WordToken base constructor
