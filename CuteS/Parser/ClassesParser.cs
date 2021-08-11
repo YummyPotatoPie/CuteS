d27d7fd26f62aab66f46cda2c1ea4e9286bc8927
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-08-12 01:44:37 +0300

    Class parser finished

--- CuteS/Parser/ClassesParser.cs
+++ CuteS/Parser/ClassesParser.cs
@@ -3,23 +3,24 @@ using System.Collections.Generic;
 using CuteS.LexicalAnalyser;
 using CuteS.LexicalAnalyser.Tokens;
 
-using CuteS.Parser.AST.Nodes;
+using CuteS.Parser.AstNodes;
+using CuteS.Parser.AstNodes.ExpressionsNodes;
+using CuteS.Parser.AstNodes.StatementsNodes;
 
 namespace CuteS.Parser
 {
     public class ClassesParser : AbstractParser<ClassesNode>
     {
-        public ClassesParser(Lexer lexer) : base(lexer) { }
+        public ClassesParser(ref Lexer lexer) : base(ref lexer) { }
 
         public override ClassesNode Parse()
         {
             List<ClassNode> classes = new();
-            ClassNode classNode = Class();
 
-            while (classNode != null)
+            while (Lex.CurrentToken.Tag != '}')
             {
-                classes.Add(classNode);
-                classNode = Class();
+                ClassNode classNode = Class();
+                if (classNode != null) classes.Add(classNode);
             }
 
             if (classes.Count == 0) throw new SyntaxError();
@@ -28,24 +29,145 @@ namespace CuteS.Parser
 
         private ClassNode Class()
         {
-            if (CurrentToken.Tag != TokenAttributes.Class) return null;
+            if (Lex.CurrentToken.Tag != TokenAttributes.Class) return null;
             else 
             {
                 IdentifierNode className;
-                Match(TokenAttributes.Class); className = ClassName(); Match('{'); /*ClassBody*/ Match('}'); 
-                return new ClassNode(className, Lex.Line);
+                ClassBodyNode classBody;
+                Match(TokenAttributes.Class); className = ClassName(); Match('{'); classBody = ClassBody(); Match('}'); 
+                return new ClassNode(className, classBody, Lex.Line);
             }
         }
 
         private IdentifierNode ClassName()
         {
-            if (CurrentToken is WordToken && CurrentToken.Tag == TokenAttributes.Identifier)
+            if (Lex.CurrentToken is WordToken token && Lex.CurrentToken.Tag == TokenAttributes.Identifier)
             {
-                string identifier = ((WordToken) CurrentToken).Lexeme;
+                string identifier = token.Lexeme;
                 Match(TokenAttributes.Identifier);
                 return new IdentifierNode(identifier, Lex.Line);
             }
             else throw new SyntaxError();
+        }
+
+        private ClassBodyNode ClassBody()
+        {
+            List<DeclarationNode> fields = new();
+            List<FunctionNode> functions = new();
+
+            while (Lex.CurrentToken.Tag != '}')
+            {
+                if (Lex.CurrentToken.Tag == TokenAttributes.Let) fields.Add(FieldDeclaration());
+                else if (Lex.CurrentToken.Tag == TokenAttributes.Function) functions.Add(FunctionDeclaration());
+                else throw new SyntaxError();
+            }
+
+            return new ClassBodyNode(fields.ToArray(), functions.ToArray(), Lex.Line);
+        }
+
+        private FunctionNode FunctionDeclaration()
+        {
+            Match(TokenAttributes.Function);
+
+            IdentifierNode functionName;
+            ArgumentNode[] arguments;
+            TypeNode returnType;
+            StatementsNode statements;
+            if (Lex.CurrentToken is WordToken token && Lex.CurrentToken.Tag == TokenAttributes.Identifier)
+            {
+                functionName = new(token.Lexeme, Lex.Line);
+                Match(TokenAttributes.Identifier);
+            }
+            else throw new SyntaxError();
+
+            Match('(');
+            arguments = Arguments();
+            Match(')');
+
+            Match(TokenAttributes.TypeOp);
+            returnType = new(TypeName(), Lex.Line);
+
+            Match('{');
+            statements = new StatementsParser(ref Lex).Parse();
+            Match('}');
+
+            return new FunctionNode(functionName, arguments, returnType, statements, Lex.Line);
+        }
+
+        private ArgumentNode[] Arguments()
+        {
+            List<ArgumentNode> arguments = new();
+            while (true)
+            {
+                IdentifierNode identifier;
+                if (Lex.CurrentToken is WordToken token && Lex.CurrentToken.Tag == TokenAttributes.Identifier) 
+                {
+                    identifier = new(token.Lexeme, Lex.Line);
+                    Match(TokenAttributes.Identifier);
+                }
+                else throw new SyntaxError();
+
+                Match(TokenAttributes.TypeOp);
+                TypeNode typeName = new(TypeName(), Lex.Line);
+
+                arguments.Add(new ArgumentNode(identifier, typeName, Lex.Line));
+
+                if (Lex.CurrentToken.Tag == ',') Match(',');
+                else break;
+            }
+
+            return arguments.ToArray();
+        }
+
+        private DeclarationNode FieldDeclaration()
+        {
+            IdentifierNode identifier;
+            TypeNode fieldType;
+            ExpressionNode expression = null;
+
+            Match(TokenAttributes.Let);
+            if (Lex.CurrentToken is WordToken token && Lex.CurrentToken.Tag == TokenAttributes.Identifier) 
+            {
+                identifier = new(token.Lexeme, Lex.Line);
+                Match(TokenAttributes.Identifier);
+            }
+            else throw new SyntaxError();
+
+            Match(TokenAttributes.TypeOp);
+            fieldType = new(TypeName(), Lex.Line);
+
+            if (Lex.CurrentToken.Tag == TokenAttributes.Assign)
+            {
+                Match(TokenAttributes.Assign);
+                expression = new ExpressionParser(ref Lex).Parse();
+            }
+            Match(';');
+
+            return new DeclarationNode(identifier, fieldType, expression, Lex.Line);
+        }
+
+        private IdExpression TypeName()
+        {
+            if (Lex.CurrentToken.Tag != TokenAttributes.Identifier) throw new SyntaxError();
+            else 
+            {
+                List<IdentifierNode> identifiers = new();
+                while (true)
+                {
+                    if (Lex.CurrentToken is WordToken token && Lex.CurrentToken.Tag == TokenAttributes.Identifier)
+                    {
+                        string identifier = token.Lexeme;
+                        identifiers.Add(new IdentifierNode(identifier, Lex.Line));
+                        Match(TokenAttributes.Identifier);
+
+                        if (Lex.CurrentToken.Tag == TokenAttributes.DotOp) Match(TokenAttributes.DotOp);
+                        else break;
+                    }
+                    else throw new SyntaxError();
+                }
+
+                return new IdExpression(identifiers.ToArray(), Lex.Line);
+            }
         }
     }
 }
