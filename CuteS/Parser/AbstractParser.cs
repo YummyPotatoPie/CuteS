2d4b19baadf6647e8239edc56ad632b5f645ee8a
YummyPotatoPie <privatepotatonoobofthecentury@gmail.com>
2021-08-12 01:45:46 +0300

    Added abstract parser class

--- CuteS/Parser/AbstractParser.cs
+++ CuteS/Parser/AbstractParser.cs
@@ -1,32 +1,23 @@
 using CuteS.LexicalAnalyser;
-using CuteS.LexicalAnalyser.Tokens;
+
+using CuteS.Parser.AstNodes;
 
 namespace CuteS.Parser
 {
-    public abstract class AbstractParser<T>
+    public abstract class AbstractParser<T> where T : Node
     {
         protected Lexer Lex;
 
-        protected Token CurrentToken;
-
-        public AbstractParser(Lexer lexer)
-        {
-            Lex = lexer;
-            CurrentToken = Lex.NextToken();
-        }
+        public AbstractParser(ref Lexer lexer) => Lex = lexer;
 
         protected void Match(int tokenTag)
         {
-            if (CurrentToken == null) throw new SyntaxError();
+            if (Lex.CurrentToken == null) throw new SyntaxError();
 
-            if (tokenTag == CurrentToken.Tag)
-            {
-                CurrentToken = Lex.NextToken();
-            }
+            if (tokenTag == Lex.CurrentToken.Tag) Lex.NextToken();
             else throw new SyntaxError();
         }
 
         public abstract T Parse();
-
     }
 }
